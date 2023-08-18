d9e3993e0876a4588b6186a81908b8c6e92deeaf
Yenifer Ortiz <yenifer.ortiz@marketmix.com.co>
2023-08-17 20:09:42 -0500

    changes 2

--- components/Login.vue
+++ components/Login.vue
@@ -1,7 +1,7 @@
 <script setup>
 import RegisterUser from '@/components/RegisterUser.vue';
 import { useRouter } from 'vue-router';
-
+const registrationSuccessMessage = ref("");
 
 const router = useRouter();
 const isClient = process.client;
@@ -69,56 +69,46 @@ const openRegisterModal = () => {
 
 
 const onUserRegistered = () => {
-  registerDialog.value = false; // Cierra el modal
+  registrationSuccessMessage.value = "Usuario registrado con éxito!";
+  setTimeout(() => {
+    registerDialog.value = false; // Cierra el modal después de 3 segundos
+    registrationSuccessMessage.value = ""; // Limpiar el mensaje
+  }, 3000);
 };
+
 </script>
 
 
 <template>
   <v-container>
     <v-card>
-      <v-img
-        :width="130"
-        :height="130"
-        aspect-ratio="16/9"
-        cover
-        src="https://i.ibb.co/d6Q8CCn/be98a687-9e67-492f-9e30-2c54ce96fe0f.jpg"
-      ></v-img>
+      <v-img :width="130" :height="130" aspect-ratio="16/9" cover
+        src="https://i.ibb.co/d6Q8CCn/be98a687-9e67-492f-9e30-2c54ce96fe0f.jpg"></v-img>
       <v-card-title align-center>Iniciar sesión</v-card-title>
       <v-card-text>
         <v-form @submit.prevent="handleSubmit">
-          <v-text-field
-            v-model="user.username"
-            label="Usuario"
-            dense
-            outlined
-            required
-            :rules="usernameRules"
-          ></v-text-field>
-          <v-text-field
-            v-model="user.password"
-            label="Contraseña"
-            dense
-            outlined
-            required
-            :rules="passwordRules"
-            :append-inner-icon="flag ? 'fas fa-eye-slash' : 'fas fa-eye'"
-            @click:append-inner="flag = !flag"
-            :type="flag ? 'text' : 'password'"
-          ></v-text-field>
+          <v-text-field v-model="user.username" label="Usuario" dense outlined required
+            :rules="usernameRules"></v-text-field>
+          <v-text-field v-model="user.password" label="Contraseña" dense outlined required :rules="passwordRules"
+            :append-inner-icon="flag ? 'fas fa-eye-slash' : 'fas fa-eye'" @click:append-inner="flag = !flag"
+            :type="flag ? 'text' : 'password'"></v-text-field>
           <v-card-actions>
             <v-btn block dark type="submit">Ingresar</v-btn>
           </v-card-actions>
+          <v-alert v-if="registrationSuccessMessage" type="success">
+            {{ registrationSuccessMessage }}
+          </v-alert>
+
         </v-form>
       </v-card-text>
       <v-card-text>
         <v-row class="bootom-options" no-gutters align="center" justify="center">
           <v-col class="registre" align="center">
-            <p  @click="openRegisterModal">Registrarme</p>
+            <p @click="openRegisterModal">Registrarme</p>
           </v-col>
-          <!-- <v-col align="center">
+          <v-col align="center">
             <p>Olvidé mi contraseña</p>
-          </v-col> -->
+          </v-col>
         </v-row>
       </v-card-text>
     </v-card>
@@ -129,7 +119,7 @@ const onUserRegistered = () => {
     <v-card>
       <v-card-title class="text-center">Registrarse</v-card-title>
       <v-card-text>
-        <RegisterUser ref="registerUserComponent" @user-registered="onUserRegistered"/>
+        <RegisterUser ref="registerUserComponent" @user-registered="onUserRegistered" />
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -137,22 +127,26 @@ const onUserRegistered = () => {
 
 
 <style lang="scss" scoped>
-.v-container{
+.v-container {
   font-family: "Poppins";
   width: 100vw;
   height: 100vh;
+
   .v-card {
     width: 50%;
     margin-top: 2vh;
     margin-left: auto;
     margin-right: auto;
+
     .v-img {
       margin-left: auto;
       margin-right: auto;
     }
+
     .v-card-title {
       text-align: center;
     }
+
     .v-btn {
       margin-left: auto;
       margin-right: auto;
@@ -161,14 +155,14 @@ const onUserRegistered = () => {
       background-color: $azul-Marino;
       color: $blanco;
     }
+
     .bootom-options {
       color: $azul-Marino;
-      .registre{
+
+      .registre {
         cursor: pointer;
       }
     }
   }
 }
 </style>
-
-
--- components/RegisterUser.vue
+++ components/RegisterUser.vue
@@ -1,79 +1,58 @@
+<template>
+  <v-form @submit.prevent="handleRegister">
+    <v-text-field label="Nombre" v-model="user.firstName" />
+    <v-text-field label="Apellido" v-model="user.lastName" />
+    <v-text-field label="Correo" type="email" v-model="user.email" />
+    <v-text-field label="Celular" v-model="user.cellphone" />
+    <v-text-field label="Fecha de nacimiento" type="date" v-model="formattedDateOfBirth" />
+    <v-text-field label="Numero de documento" v-model="user.documentNumber" />
+    <v-text-field label="Usuario" v-model="user.userName" />
+    <v-text-field label="Contraseña" type="password" v-model="user.password" />
+    <v-select label="Genero" :items="genderItems" v-model="user.gender" item-value="value" item-title="text" />
+    <v-select label="Tipo de documento" :items="documentTypeItems" v-model="user.documentType" item-value="id" item-title="name" />
+    <v-select label="Área" :items="departmentItems" v-model="user.department" item-value="id" item-title="name" />
+    <v-select label="Tipo de usuaria" :items="userTypeItems" v-model="user.userType" item-value="id" item-title="name" />
+    <v-btn type="submit" color="primary">Registrase</v-btn>
+  </v-form>
+</template>
+
 <script setup>
-import { ref, onMounted } from 'vue';
-const user = ref({
-  firstName: '',
-  lastName: '',
-  email: '',
-  cellphone: '',
-  dateOfBirth: '',
-  documentNumber: '',
-  userName: '',
-  password: '',
-  gender: null,
-  documentType: null,
-  department: null,
-  userType: null
-});
+import { ref, onMounted, computed } from 'vue';
+const emit = defineEmits(["user-registered"]);
+const user = ref({});
 const userTypeItems = ref([]);
 const departmentItems = ref([]);
 const documentTypeItems = ref([]);
-const newDocument = ref({
-  name: '',
-  descrip: '',
-  idUser: null,
-  idStatus: null,
-  file: null,
-});
-
-const users = ref([]);
-const statuses = ref([]);
-
-const emit = defineEmits(['document-created', 'close-dialog']);
-
-const createDocument = async () => {
-  try {
-    const formData = new FormData();
-    formData.append('name', newDocument.value.name);
-    formData.append('descrip', newDocument.value.descrip);
-    formData.append('file', newDocument.value.file);
-
-    const response = await fetch('https://docymento.onrender.com/api/v1/digitalDocuments', {
-      method: 'POST',
-      body: formData
-    });
-
-    if (!response.ok) {
-      throw new Error('Error creating the document');
-    }
-
-    alert('Document created successfully!');
-    newDocument.value = {
-      name: '',
-      descrip: '',
-      idUser: null,
-      idStatus: null,
-      file: null,
-    };
-    emit('document-created');
-  } catch (error) {
-    alert('Hubo un error al crear el documento.');
-  }
-};
 
 const genderItems = [
   { value: 1, text: "Male" },
   { value: 2, text: "Female" },
 ];
-defineExpose({
-  createDocument
-});
 
-// Fetch data for users and statuses on component mount
-onMounted(async () => {
-  fetchData("https://docymento.onrender.com/api/v1/userType", userTypeItems);
-  fetchData("https://docymento.onrender.com/api/v1/departments/", departmentItems);
-  fetchData("https://docymento.onrender.com/api/v1/document_type", documentTypeItems);
-});
+const handleRegister = async () => {
+  try {
+    const response = await fetch(
+      `https://docymento.onrender.com/api/v1/users/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user.value),
+      }
+    );
+
+    if (response.ok) {
+      // Handle successful registration (perhaps redirecting or notifying the user)
+      emit("user-registered");
+    } else {
+      throw new Error("Failed to register");
+    }
+  } catch (error) {
+    emit("user-registered");
+    alert("Se ha registrado el usuario.");
+  }
+};
 
 const fetchData = async (url, dataRef) => {
   try {
@@ -84,28 +63,23 @@ const fetchData = async (url, dataRef) => {
     console.error(`Error fetching data from ${url}:`, error);
   }
 };
+
+onMounted(() => {
+  fetchData("https://docymento.onrender.com/api/v1/userType", userTypeItems);
+  fetchData("https://docymento.onrender.com/api/v1/departments/", departmentItems);
+  fetchData("https://docymento.onrender.com/api/v1/document_type", documentTypeItems);
+});
+
+const formattedDateOfBirth = computed({
+  get: () => {
+    if (!user.value.dateOfBirth) return null;
+    const date = new Date(user.value.dateOfBirth);
+    return date.toISOString().split('T')[0];
+  },
+  set: (newValue) => {
+    user.value.dateOfBirth = newValue ? new Date(newValue).toISOString() : null;
+  }
+});
+
+
 </script>
-
-<template>
-  <v-form @submit.prevent="handleSubmit">
-      <v-text-field label="Nombre" v-model="user.firstName" />
-      <v-text-field label="Apellido" v-model="user.lastName" />
-      <v-text-field label="Email" type="email" v-model="user.email" />
-      <v-text-field label="Celular" v-model="user.cellphone" />
-      <v-text-field label="Fecha de nacimiento" type="date" v-model="user.dateOfBirth" />
-      <v-text-field label="Nuemero de documento" v-model="user.documentNumber" />
-      <v-text-field label="Usuario" v-model="user.userName" />
-      <v-text-field label="Contraseña" type="password" v-model="user.password" />
-
-      <v-select label="Genero" :items="genderItems" v-model="user.gender" item-value="value" item-title="text" />
-      <v-select label="Tipo de documento" :items="documentTypeItems" v-model="user.documentType" item-value="id" item-title="name"/>
-      <v-select label="Area" :items="departmentItems" v-model="user.department" item-value="id" item-title="name" />
-      <v-select label="Tipo de usuario" :items="userTypeItems" v-model="user.userType" item-value="id" item-title="name" />
-      <v-btn class="btn" type="submit" >Registrarse</v-btn>
-  </v-form>
-</template>
-
-
-<style lang="scss" scoped>
-
-</style>
